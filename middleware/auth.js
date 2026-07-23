7ac5bfbe7bc434d2720bff6b85b86f77ffdca3e1
Elirus <thedeveloperpauksa@gmail.com>
2026-07-23 22:49:30 +0630

    JWT token authentication started

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,3 +1,41 @@
-async function verify(req, res, next) {
-    
+const jwt = require("jsonwebtoken");
+const {config} = require("../config/Config");
+async function verify(req, res,next) {
+    console.log('verify ');
+    let authorization = req.headers.authorization;
+    if (!authorization) {
+        res.status(401).json({
+            message:'Not authorized'
+        });
+    }
+    else {
+        let token = authorization.substring("Bearer ".length);
+        if(token )
+        {
+            //console.log('jwt token ',token);
+            try
+            {
+                let payload = await jwt.verify(token, config.TOKEN_SECRET);
+                console.log('jwt payload ',payload);
+                req.user = payload;
+                next();
+            }
+            catch(e)
+            {
+                console.error(e);
+                res.status(401).json({
+                    message:'Not authorized'
+                });
+            }
+
+        }
+        else
+        {
+
+            res.status(401).json({
+                message:'Not authorized'
+            });
+        }
+
+    }
 }
